39c569b5c66f0716951bad546b69f0c42e7c5295
Barry Awure <awure_barry@yahoo.com>
2024-08-23 23:45:47 +0000

    create bucket updated with user input

--- s3/sdk/python/create-bucket.py
+++ s3/sdk/python/create-bucket.py
@@ -2,45 +2,26 @@ import logging
 import boto3
 from botocore.exceptions import ClientError
 
-def create_bucket(bucket_name, region=None):
-    """Create an S3 bucket in a specified region
+import logging
+import boto3
+from botocore.exceptions import ClientError
 
-    If a region is not specified, the bucket is created in the S3 default
-    region (us-east-1).
+client = boto3.client('s3')
 
-    :param bucket_name: Bucket to create
-    :param region: String region to create bucket in, e.g., 'us-west-2'
-    :return: True if bucket created, else False
-    """
+bucket_name = input("Enter Bucket Name: ")
+#region = input("Enter Region: ")  # Optional, specify your desired region
 
-    # Create bucket
-    try:
-        if region is None:
-            s3_client = boto3.client('s3')
-            s3_client.create_bucket(Bucket=bucket_name)
-        else:
-            s3_client = boto3.client('s3', region_name=region)
-            location = {'LocationConstraint': region}
-            s3_client.create_bucket(Bucket=bucket_name,
-                                    CreateBucketConfiguration=location)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
-
-
-"""
-response = client.create_bucket(
-    Bucket='examplebucket',
-    CreateBucketConfiguration={
-        'LocationConstraint': 'eu-west-1',
-    },
-)
-
-print(response)
-"""
-# Usage
-bucket_name = 'imex-bucket-name-345'
-region = 'us-west-1'  # Optional, specify your desired region
-
-create_bucket(bucket_name, region)
+try:
+    response = client.create_bucket(
+        Bucket=bucket_name,
+        CreateBucketConfiguration={
+            'LocationConstraint': region,
+        },
+    )
+    print(f"Bucket '{bucket_name}' created successfully.")
+except ClientError as e:
+    logging.error(e)
+    print(f"Error creating bucket: {e}")
+else:
+    # Print the bucket name
+    print(f"Created bucket name: {bucket_name}")

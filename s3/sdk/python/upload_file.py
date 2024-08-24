c25ea4c9a7f6309c0fe5921a365af8bde1852d87
Barry Awure <awure_barry@yahoo.com>
2024-08-24 01:19:22 +0000

    callback parameter

--- s3/sdk/python/upload_file.py
+++ s3/sdk/python/upload_file.py
@@ -1,5 +1,9 @@
 import boto3
 s3 = boto3.client('s3')
-s3.upload_file(r'C:\Users\BarryAwure\Downloads\HOP School of HArvest (HSH) Training Manual - Module 1.pdf', 'my-unique-bucket-name-49', 'HOP School of HArvest (HSH) Training Manual - Module 1.pdf')
+s3.upload_file(
+    r'C:\Users\BarryAwure\Downloads\HOP School of HArvest (HSH) Training Manual - Module 1.pdf', 
+    'my-unique-bucket-name-49', 'HOP School of HArvest (HSH) Training Manual - Module 1.pdf',
+    Callback=ProgressPercentage('HOP School of HArvest (HSH) Training Manual - Module 1.pdf')
+)
 
 
